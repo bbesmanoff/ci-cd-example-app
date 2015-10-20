3273bdfef20015643fd943379633690107926824
Brian Besmanoff <bbesmanoff@gmail.com>
2015-10-20 11:09:54 -0400

    Add basic functions

---
+++ index.js
@@ -0,0 +1,23 @@
+export function abs(val) {
+  if (val <= 0) {
+    return -val;
+  }
+
+  return val;
+};
+
+export function min(v1, v2) {
+  if (v1 <= v2) {
+    return v1;
+  }
+
+  return v2;
+};
+
+export function max(v1, v2) {
+  if (v1 >= v2) {
+    return v1;
+  }
+
+  return v2;
+};
